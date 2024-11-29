1483df333bfffc7891934de9df80c64e028be73b
[sathvika muktha] <[sathvikamuktha@gmail.com]>
2024-11-29 17:08:46 -0500

    added initial templates

--- base/views.py
+++ base/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
+rooms = [
+    {'id':1, 'name' : 'Job Posting page!'},
+    {'id':2, 'name' : 'Profile settings '},
+    {'id':3, 'name' : 'Feature 1'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 def room(request):
     return render(request, 'room.html')
