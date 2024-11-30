1c57809f4e5ef1e995f8c7ed922017683215fc42
[sathvika muktha] <[sathvikamuktha@gmail.com]>
2024-11-30 17:37:59 -0500

    added navbar & search functions

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 # from django.http import HttpResponse
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 # rooms = [
@@ -10,8 +11,20 @@ from .forms import RoomForm
 # ]
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    #search function to seach using keywords
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+
+    topics = Topic.objects.all()
+    room_count = rooms.count() #use for total no. of jobs
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
