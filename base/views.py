ad5a616a7187d9ab9c96143e8f80625a5e5a6225
[sathvika muktha] <[sathvikamuktha@gmail.com]>
2024-11-30 13:35:02 -0500

    added database
    added users, jobs, messages, rooms, etc

--- base/views.py
+++ base/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from .models import Room
 
-rooms = [
-    {'id':1, 'name' : 'Job Posting page!'},
-    {'id':2, 'name' : 'Profile settings '},
-    {'id':3, 'name' : 'Feature 1'},
-]
+# rooms = [
+#     {'id':1, 'name' : 'Job Posting page!'},
+#     {'id':2, 'name' : 'Profile settings '},
+#     {'id':3, 'name' : 'Feature 1'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room' : room}
     return render(request, 'base/room.html', context)
